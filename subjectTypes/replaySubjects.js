68b376e637bab170425617bc4ac1c6e7663ed5ff
vineet.dev <vineet.dev@ril.com>
2017-10-08 03:18:33 +0530

    subject types done
    Signed-off-by: vineet.dev <vineet.dev@ril.com>

---
+++ subjectTypes/replaySubjects.js
@@ -0,0 +1,30 @@
+var source = Rx.Observable.interval(500);
+var subject = new Rx.ReplaySubject(2);
+var refCounted = source.multicast(subject).refCount();
+var subscription1, subscription2, subscriptionConnect;
+
+// This calls `connect()`, because
+// it is the first subscriber to `refCounted`
+console.log('observerA subscribed');
+subscription1 = refCounted.subscribe({
+  next: (v) => console.log('observerA: ' + v)
+});
+
+setTimeout(() => {
+  console.log('observerB subscribed');
+  subscription2 = refCounted.subscribe({
+    next: (v) => console.log('observerB: ' + v)
+  });
+}, 1200);
+
+setTimeout(() => {
+  console.log('observerA unsubscribed');
+  subscription1.unsubscribe();
+}, 1500);
+
+// This is when the shared Observable execution will stop, because
+// `refCounted` would have no more subscribers after this
+setTimeout(() => {
+  console.log('observerB unsubscribed');
+  subscription2.unsubscribe();
+}, 2000);
