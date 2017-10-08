45e914639c326d2d90a9fcd8d9899f01e9ea19d8
vineet.dev <vineet.dev@ril.com>
2017-10-08 12:10:53 +0530

    added functionality to multiply by two
    Signed-off-by: vineet.dev <vineet.dev@ril.com>

--- React/simple-react/src/App.js
+++ React/simple-react/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super(props);
     this.state = {times: []};
     this.stopTime = this.stopTime.bind(this);
+    this.multiplyByTwo = this.multiplyByTwo.bind(this);
   }
   componentDidMount() {
     this.times = times
@@ -22,6 +23,15 @@ class App extends Component {
   stopTime() {
     this.times.unsubscribe();
   }
+  multiplyByTwo() {
+    this.stopTime();
+    this.times = times
+      .map(times => times * 2)
+      // Accumulate our times in an array
+      .scan((times, message) => [message].concat(times), [])
+      // And render whenever we get a new message
+      .subscribe(times => this.setState({times: times}));
+  }
   render() {
     return (
       <div className="App">
@@ -34,6 +44,7 @@ class App extends Component {
           {this.state.times.map(message => <li key={message}>{message}</li>)}
         </ul>
         <button onClick={this.stopTime}>Stop It</button>
+        <button onClick={this.multiplyByTwo}>Multiply by 2</button>
       </div>
       </div>
     );
