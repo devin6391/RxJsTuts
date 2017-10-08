12e31bfefa75f7acdbab51f873d6269504cc2640
vineet.dev <vineet.dev@ril.com>
2017-10-08 12:05:51 +0530

    examples and simple react example shown
    Signed-off-by: vineet.dev <vineet.dev@ril.com>

---
+++ React/simple-react/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import times from "./observable";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {times: []};
+    this.stopTime = this.stopTime.bind(this);
+  }
+  componentDidMount() {
+    this.times = times
+      // Accumulate our times in an array
+      .scan((times, message) => [message].concat(times), [])
+      // And render whenever we get a new message
+      .subscribe(times => this.setState({times: times}));
+  }
+  componentWillUnmount() {
+    this.times.unsubscribe();
+  }
+  stopTime() {
+    this.times.unsubscribe();
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Start watch</h1>
+        </header>
+        <div>
+        <ul>
+          {this.state.times.map(message => <li key={message}>{message}</li>)}
+        </ul>
+        <button onClick={this.stopTime}>Stop It</button>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ React/simple-react/src/observable.js
@@ -0,0 +1,4 @@
+import { Observable } from "rxjs";
+
+let obs = new Observable.interval(1000);
+export default obs;
